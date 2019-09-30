ccc7776d99f766d4ca44d6e471152d0386fbc405
ungidrid <denisdoskach@gmail.com>
2019-09-30 12:48:41 +0300

    Revert

--- src/IntelligentCooking/IntelligentCooking.ConsoleDatabaseDemo/Program.cs
+++ src/IntelligentCooking/IntelligentCooking.ConsoleDatabaseDemo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using IntelligentCooking.Core.Interfaces.UnitsOfWork;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,33 +20,8 @@ namespace IntelligentCooking.ConsoleDatabaseDemo
 
         public async Task Run()
         {
-            Console.WriteLine("All Categories: \n");
-            var categories = await _unitOfWork.Categories.Get();
-            foreach (var category in categories)
-            {
-                Console.WriteLine("\tId : " + category.Id + "\tName: " + category.Name);
-            }
-
-            Console.WriteLine("\nAll Ingredients: \n");
-            var ingredients = await _unitOfWork.Ingredients.Get();
-            foreach (var ingredient in ingredients)
-            {
-                Console.WriteLine("\tId: " + ingredient.Id + "\tName: " + ingredient.Name + "\tDescription: " + ingredient.Description);
-            }
-
-            Console.WriteLine("\nAll Dishes: ");
-            var dishes = await _unitOfWork.Dishes.Get();
-            foreach (var dish in dishes)
-            {
-                Console.WriteLine("\n\tId: " + dish.Id + "\n\tName: " + dish.Name + "\n\tImageUrl: " + dish.ImageUrl + "\n\tRecipe: " + dish.Recipe + "\n\tTime: " + dish.Time + "\n\tServings: " + dish.Servings + "\tStars: " + dish.Stars + "\tProteins: " + dish.Proteins + "\tFats: " + dish.Fats + "\tCarbohydrates: " + dish.Carbohydrates + "\tCalories: " + dish.Calories + "\n\tDescription: " + dish.Description);
-            }
-
-            Console.WriteLine("\nAll DishIngredients: \n");
-            var dishIngredients = await _unitOfWork.DishIngredients.Get();
-            foreach (var dishIngredient in dishIngredients)
-            {
-                Console.WriteLine("\tId: " + dishIngredient.Id + "\tAmount: " + dishIngredient.Amount);
-            }
+            var category = await _unitOfWork.Categories.FindAsync(1);
+            Console.WriteLine(category.Name);
         }
     }
 
@@ -56,8 +30,8 @@ namespace IntelligentCooking.ConsoleDatabaseDemo
         static void Main(string[] args)
         {
             var demo = new DataBaseDemo();
-
             demo.Run();
+
             Console.ReadLine();
         }
     }

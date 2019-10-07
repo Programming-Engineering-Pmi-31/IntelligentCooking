4ba0c128c3ede7130f84fee9d5047f46567410d6
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-10-07 16:01:47 +0300

    ...

--- src/client/src/actions/ingridients.js
+++ src/client/src/actions/ingridients.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export const setIngridients = () => dispatch => {
     let response = axios
-        .get("/ingridients.json")
+        .get("https://localhost:5001/api/ingredient")
         .then(res => {dispatch({type: 'SET_INGRIDIENTS',payload: res.data})})
 
 };

8668b876a6257d86f5d17f60db8392c6a92f57f6
ungidrid <denisdoskach@gmail.com>
2019-11-14 20:55:38 +0200

    fixed

--- src/IntelligentCooking/InelligentCooking.BLL/DTOs/IngredientDetailedDto.cs
+++ src/IntelligentCooking/InelligentCooking.BLL/DTOs/IngredientDetailedDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace InelligentCooking.BLL.DTOs
 {
-    public class DetailedIngredientDto
+    public class IngredientDetailedDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
